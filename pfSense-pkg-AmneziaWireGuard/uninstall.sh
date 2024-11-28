a5f282c17f3f17590410efd98cbb8d9e3e9656ca
pacitwizere@hotmail.com <pacitwizere@hotmail.com>
2024-11-28 11:51:03 +0200

    changing package name

---
+++ pfSense-pkg-AmneziaWireGuard/uninstall.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# Uninstall script for pfSense-pkg-AmneziaWireGuard
+
+echo "Uninstalling Amnezia-WireGuard package..."
+
+# Remove files
+echo " - Removing files..."
+rm -f /etc/inc/priv/amneziawireguard.priv.inc 
+rm -f /usr/local/pkg/amneziawireguard*.xml
+rm -f /usr/local/pkg/amneziawireguard/*.*
+rm -f /usr/local/pkg/amneziawireguard/classes/*.*
+rm -f /usr/local/pkg/amneziawireguard/includes/*.*
+rm -f /usr/local/share/pfSense-pkg-AmneziaWireGuard/*.xml
+rm -f /usr/local/www/awg/js/*.js
+rm -rf /usr/local/www/awg/*
+rm -rf /usr/local/www/shortcuts/*
+
+# Remove directories (only if empty, to avoid unintended deletions)
+echo " - Removing directories..."
+rmdir /etc/inc/priv
+rmdir /usr/local/pkg/amneziawireguard/classes
+rmdir /usr/local/pkg/amneziawireguard/includes
+rmdir /usr/local/share/pfSense-pkg-AmneziaWireGuard
+rmdir /usr/local/www/awg/js
+rmdir /usr/local/www/widgets
+rmdir /usr/local/www/shortcuts
+
+echo "Uninstallation complete."
