e2a4df1ef989a3d2c63417f73bfceca281da67a8
Twizere <ptwizere@qtsoftwareltd.com>
2025-02-25 16:12:19 +0200

    renaming again

--- pfSense-pkg-AmneziaWireGuard/uninstall.sh
+++ pfSense-pkg-AmneziaWireGuard/uninstall.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Uninstall script for pfSense-pkg-AmneziaWireGuard
+# Uninstall script for pfSense-pkg-Amneziawireguard
 
 echo "Uninstalling Amnezia-WireGuard package..."
 
@@ -10,7 +10,7 @@ rm -f /usr/local/pkg/amneziawireguard*.xml
 rm -f /usr/local/pkg/amneziawireguard/*.*
 rm -f /usr/local/pkg/amneziawireguard/classes/*.*
 rm -f /usr/local/pkg/amneziawireguard/includes/*.*
-rm -f /usr/local/share/pfSense-pkg-AmneziaWireGuard/*.xml
+rm -f /usr/local/share/pfSense-pkg-Amneziawireguard/*.xml
 rm -f /usr/local/www/awg/js/*.js
 rm -rf /usr/local/www/awg/*
 rm -rf /usr/local/www/shortcuts/*
@@ -20,7 +20,7 @@ echo " - Removing directories..."
 rmdir /etc/inc/priv
 rmdir /usr/local/pkg/amneziawireguard/classes
 rmdir /usr/local/pkg/amneziawireguard/includes
-rmdir /usr/local/share/pfSense-pkg-AmneziaWireGuard
+rmdir /usr/local/share/pfSense-pkg-Amneziawireguard
 rmdir /usr/local/www/awg/js
 rmdir /usr/local/www/widgets
 rmdir /usr/local/www/shortcuts
