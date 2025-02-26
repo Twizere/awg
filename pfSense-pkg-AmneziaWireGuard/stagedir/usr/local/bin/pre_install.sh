e6429ce90fdaeca67ac20b0ea361982ccc6491e7
Twizere <ptwizere@qtsoftwareltd.com>
2025-02-26 13:23:32 +0200

    making the presecript

--- pfSense-pkg-AmneziaWireGuard/stagedir/usr/local/bin/pre_install.sh
+++ pfSense-pkg-AmneziaWireGuard/stagedir/usr/local/bin/pre_install.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
-rm -f /var/db/pkg/*.lock
-pkg install -y bash
+
+# Log that post_install is being executed
+echo "Running post-installation tasks..." 
+# Check if bash is installed
+if ! pkg info -e bash; then
+  echo "bash is not installed, Install it first and try again later."
+  #pkg install -y bash >> /var/log/messages 2>&1
+
+else
+  echo "bash is already installed."
