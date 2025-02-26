86d076bb9f88002e5803792a45c4ad2ae656d72b
Twizere <ptwizere@qtsoftwareltd.com>
2025-02-26 13:39:21 +0200

    Trying red text

--- pfSense-pkg-AmneziaWireGuard/stagedir/usr/local/bin/post_install.sh
+++ pfSense-pkg-AmneziaWireGuard/stagedir/usr/local/bin/post_install.sh
@@ -5,9 +5,9 @@ echo "Running post-installation tasks..."
 
 # Check if bash is installed, suppressing output for a clean check
 if ! pkg info -e bash > /dev/null 2>&1; then
-  echo "Error: bash is not installed."
-  echo "Amnezia Wireguard (awg-quick) may not run properly."
-  echo "Please install bash and try again."
+  printf "\033[31mError: bash is not installed.\033[0m\n"
+  printf "\033[31mAmnezia Wireguard (awg-quick) may not run properly.\033[0m\n"
+  printf "\033[31mPlease install bash and try again.\033[0m\n"
   # Optionally, you can auto-install bash by uncommenting the following line:
   # pkg install -y bash >> /var/log/messages 2>&1
   exit 1
