fe65fe2f224c1101765bfa9821f0833a625ab3fe
Twizere <ptwizere@qtsoftwareltd.com>
2025-02-27 17:02:17 +0200

    modifying the file

--- pfSense-pkg-AmneziaWireGuard/files/usr/local/www/awg/awg_process.php
+++ pfSense-pkg-AmneziaWireGuard/files/usr/local/www/awg/awg_process.php
@@ -9,7 +9,6 @@ require_once("certs.inc");
 $pgtitle = array("VPN", "Amenezia WG");
 include("head.inc");
 
- 
 $tab_array = array();
 $tab_array[] = array(gettext("Server"), true, "index.php");
 $tab_array[] = array(gettext("Clients"), false, "?tab=clients");
@@ -21,35 +20,143 @@ display_top_tabs($tab_array);
 $wg_config_file = "/etc/amnezia/amneziawg/awg0.conf";
 $wg_config = file_exists($wg_config_file) ? file_get_contents($wg_config_file) : "Configuration file not found.";
 
-$server_config = "";
-$peers_config = "";
-$parsing_peers = false;
+// Initialize variables for each configuration
+$privateKey = '';
+$listenPort = '';
+$jC = '';
+$jMin = '';
+$jMax = '';
+$s1 = '';
+$s2 = '';
+$h1 = '';
+$h2 = '';
+$h3 = '';
+$h4 = '';
 
+// Parse the configuration file
 foreach (explode("\n", $wg_config) as $line) {
-    if (trim($line) === "[Peer]") {
-        $parsing_peers = true;
+    // Extract values from the configuration file
+    if (strpos($line, 'PrivateKey') !== false) {
+        $privateKey = trim(substr($line, strpos($line, '=') + 1));
     }
-    
-    if ($parsing_peers) {
-        $peers_config .= htmlspecialchars($line) . "<br>";
-    } else {
-        $server_config .= htmlspecialchars($line) . "<br>";
+    if (strpos($line, 'ListenPort') !== false) {
+        $listenPort = trim(substr($line, strpos($line, '=') + 1));
+    }
+    if (strpos($line, 'Jc') !== false) {
+        $jC = trim(substr($line, strpos($line, '=') + 1));
+    }
+    if (strpos($line, 'Jmin') !== false) {
+        $jMin = trim(substr($line, strpos($line, '=') + 1));
+    }
+    if (strpos($line, 'Jmax') !== false) {
+        $jMax = trim(substr($line, strpos($line, '=') + 1));
+    }
+    if (strpos($line, 'S1') !== false) {
+        $s1 = trim(substr($line, strpos($line, '=') + 1));
+    }
+    if (strpos($line, 'S2') !== false) {
+        $s2 = trim(substr($line, strpos($line, '=') + 1));
+    }
+    if (strpos($line, 'H1') !== false) {
+        $h1 = trim(substr($line, strpos($line, '=') + 1));
+    }
+    if (strpos($line, 'H2') !== false) {
+        $h2 = trim(substr($line, strpos($line, '=') + 1));
+    }
+    if (strpos($line, 'H3') !== false) {
+        $h3 = trim(substr($line, strpos($line, '=') + 1));
+    }
+    if (strpos($line, 'H4') !== false) {
+        $h4 = trim(substr($line, strpos($line, '=') + 1));
     }
 }
 
-// Display the server configuration
-echo "<div class='panel panel-primary'>";
-echo "<div class='panel-heading'><h2 class='panel-title'>Server Configuration</h2></div>";
-echo "<div class='panel-body'><pre>{$server_config}</pre></div>";
-echo "</div>";
+// Create the form
+$form = new Form();
 
-// Display the peers configuration if on Clients tab
-if ($_GET['tab'] === "clients") {
-    echo "<div class='panel panel-success'>";
-    echo "<div class='panel-heading'><h2 class='panel-title'>Client Peers</h2></div>";
-    echo "<div class='panel-body'><pre>{$peers_config}</pre></div>";
-    echo "</div>";
-}
+// Server Configuration Section
+$section = new Form_Section('Server Configuration');
+$section->addInput(new Form_Input(
+    'private_key',
+    '*Private Key',
+    'text',
+    $privateKey
+))->setHelp('Enter the Private Key for the server.');
 
+$section->addInput(new Form_Input(
+    'listen_port',
+    '*Listen Port',
+    'text',
+    $listenPort
+))->setHelp('Port for the server to listen on.');
+
+$section->addInput(new Form_Input(
+    'jc',
+    'Jc',
+    'text',
+    $jC
+))->setHelp('Value for Jc.');
+
+$section->addInput(new Form_Input(
+    'jmin',
+    'Jmin',
+    'text',
+    $jMin
+))->setHelp('Value for Jmin.');
+
+$section->addInput(new Form_Input(
+    'jmax',
+    'Jmax',
+    'text',
+    $jMax
+))->setHelp('Value for Jmax.');
+
+$section->addInput(new Form_Input(
+    's1',
+    'S1',
+    'text',
+    $s1
+))->setHelp('Value for S1.');
+
+$section->addInput(new Form_Input(
+    's2',
+    'S2',
+    'text',
+    $s2
+))->setHelp('Value for S2.');
+
+$section->addInput(new Form_Input(
+    'h1',
+    'H1',
+    'text',
+    $h1
+))->setHelp('Value for H1.');
+
+$section->addInput(new Form_Input(
+    'h2',
+    'H2',
+    'text',
+    $h2
+))->setHelp('Value for H2.');
+
+$section->addInput(new Form_Input(
+    'h3',
+    'H3',
+    'text',
+    $h3
+))->setHelp('Value for H3.');
+
+$section->addInput(new Form_Input(
+    'h4',
+    'H4',
+    'text',
+    $h4
+))->setHelp('Value for H4.');
+
+// Add section to the form
+$form->add($section);
+
+// Display the form
+print($form);
 include("foot.inc");
 ?>
