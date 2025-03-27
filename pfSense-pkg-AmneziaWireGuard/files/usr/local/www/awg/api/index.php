b0ab3e5b91ee73ba4eca686a5fd1c7eeeb55c7f4
Twizere <ptwizere@qtsoftwareltd.com>
2025-03-27 10:06:19 +0200

    modifying the rules

--- pfSense-pkg-AmneziaWireGuard/files/usr/local/www/awg/api/index.php
+++ pfSense-pkg-AmneziaWireGuard/files/usr/local/www/awg/api/index.php
@@ -253,9 +253,10 @@ function applyFirewallRules($interface, $ipCidr)
     // Create or update the PF configuration file
     $pfConf = <<<EOT
         set skip on lo0
-        nat on vtnet0 from {$interface}:network to any -> (vtnet0)
+        nat on vtnet0 from {$ipCidr} to any -> (vtnet0)
         pass in on {$interface} from any to {$ipCidr} keep state
         pass out on vtnet0 from {$ipCidr} to any keep state
+        pass in on {$interface}  inet from {$ipCidr}  to (self) keep state
         EOT;
 
     file_put_contents('/etc/pf.conf', $pfConf);
